ba320c0782392fb109209188d7c707f500003161
EZ2405 <169991529+EZA2405@users.nireply.github.com>
2025-11-26 17:57:18 +0800

    initial commit

---
+++ face.js
@@ -0,0 +1,69 @@
+const webcamElement = document.getElementById("webcam");
+const memeImage = document.getElementById("memeImage");
+
+
+async function setupWebcam() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: true;
+        audio: false;
+    });
+
+    webcamElement.srcObject = stream;
+
+    return new Promise(resolve => {
+        webcamElement.onloadedmetadata = () => {
+            resolve();
+        };
+    });
+};
+
+await setupWebcam();
+const face_model = await tmImage.load("tm-model/model.json","tm-model/metadata.json");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+predictLoop();
+
+async function predictLoop() {
+    ctx.drawImage(webcamElement, 0, 0, canvas.width, canvas.height);
+
+    const prediction = await face_model.predict(canvas);
+
+    let highestProb = 0;
+    let predicted_class = "";
+
+    prediction.forEach(p => {
+        if (p.probability > highestProb) {
+            highestProb = p.probability;
+            predicted_class = p.className;
+        }
+    });
+
+    switch (predicted_class) {
+        case "mouth":
+            memeImage.src = "images/mouth.jpg"
+            break;
+        case "finger":
+            memeImage.src = "images/finger.jpg"
+            break;
+        case "neutral":
+            memeImage.src = "images/neutral.jpg"
+            break;
+        case "scared":
+            memeImage.src = "images/scared.jpg"
+            break;
+        case "thumb":
+            memeImage.src = "images/thumb.jpg"
+            break;
+        case "lipbite":
+            memeImage.src = "images/lipbite.jpg"
+            break;
+        case "shocked":
+            memeImage.src = "images/shocked.jpg"
+            break;
+        default:
+            memeImage.src = "";
+    }
+
+    requestAnimationFrame(predictLoop);
+}
